fef7aef0dee6b97571cbefb8a94007e354c09e2f
Zhansaya <zhansaiabaikenova@gmail.com>
2026-03-03 13:10:48 +0500

    Удалила лишние коментарий в старых файлах(TODO and и т.д)

--- src/com/narxoz/rpg/hero/character/Warrior.java
+++ src/com/narxoz/rpg/hero/character/Warrior.java
@@ -13,31 +13,4 @@ public class Warrior extends BaseCh {
         System.out.println(name + "uses Berserk! Strength increased temporarily!");
     }
 
-// Example concrete implementation of a Character.
-
-// This is provided as a reference to help you get started.
-// Study this implementation, then create similar classes for Mage, Archer, etc.
-
-// Notice:
-// How attributes are initialized
-// How methods are implemented
-// The structure you should follow for other character types
-
-// TODO: Create similar implementations for:
-// Mage (high mana/intelligence, low health/strength)
-// Archer (balanced stats, ranged combat)
-// (Optional) Additional classes: Rogue, Paladin, etc.
-// TODO: Add fields for equipped weapon and armor
-    // Think: Should Warrior know about its equipment?
-
-
-// TODO: Implement methods from Character interface
-    // You need to define those methods in Character interface first!
-
-    // Example method structure:
-// TODO: Add equipment-related methods
-    // Examples:
-    // - void equipWeapon(Weapon weapon)
-    // - void equipArmor(Armor armor)
-    // - void displayEquipment()
 }
--- src/com/narxoz/rpg/hero/equipment/Weapon.java
+++ src/com/narxoz/rpg/hero/equipment/Weapon.java
@@ -7,26 +7,4 @@ public interface Weapon {
     String getSpecialEffect();
     //Можно потом добавить типа историю оружия или и т.д если не будет лень
 
-
-
-
-    /**
-     * Interface for all weapon types in the game.
-     *
-     * Abstract Factory Pattern:
-     * This is one of the "Product" interfaces in an Abstract Factory.
-     * Each equipment theme (Medieval, Magic, Ranger) will have its own concrete weapon.
-     *
-     * TODO: Define what all weapons have in common
-     * Think: What information should every weapon provide?
-     * Examples: damage, weapon type, special properties, etc.
-     */
-
-    // TODO: Add weapon behavior methods
-    // Consider:
-    // - int getDamage()
-    // - String getWeaponInfo()
-    // - String getWeaponType()
-    // - any theme-specific properties?
-
 }
